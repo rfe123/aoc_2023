2e48e5cd687c8b24748a227104dd073394db0f4f
rfe123 <59402267+rfe123@users.noreply.github.com>
2024-09-25 00:14:36 -0500

    Update to find word digits and numeric digits

--- challenge_01/challenge.py
+++ challenge_01/challenge.py
@@ -1,20 +1,52 @@
 f = open("input.txt", "r")
 
+words =  [None,'one','two','three','four','five','six','seven','eight','nine']
+
 lines = []
 
 for x in f:
+  print(x)
+  first_idx = len(x) + 999
   first = None
+  last_idx = -1
   last = None
 
-  for c in x:
-    if c.isnumeric():
-        if first == None:
-           first = c
-        last = c
+  for i in range(1,10):
+    dig_idx = x.find(str(i))
+    wrd_idx = x.find(words[i])
+
+    if dig_idx > -1:
+      if dig_idx < first_idx:
+        first_idx = dig_idx
+        first = str(i)
+
+    if wrd_idx > -1:
+      if wrd_idx < first_idx:
+        first_idx = wrd_idx
+        first = i
+  
+    dig_idx = x.rfind(str(i))
+    wrd_idx = x.rfind(words[i])
+
+    if dig_idx > -1:
+      if dig_idx > last_idx:
+        last_idx = dig_idx
+        last = str(i)
+
+    if wrd_idx > -1:
+      if wrd_idx > last_idx:
+        last_idx = wrd_idx
+        last = i
+      
+    print(f'first={first} last={last}')
 
   if first != None:
     line = int(f"{first}{last}")
-    print(line)
+    print(f"{x} : {line}")
     lines.append(line)
+
+  
+  # break
+  
 
 print(sum(lines))
