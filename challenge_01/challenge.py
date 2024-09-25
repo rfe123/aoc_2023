557c703ebac2b7801dbe8f33edceebcbc2c2cbd1
rfe123 <59402267+rfe123@users.noreply.github.com>
2024-09-25 00:18:19 -0500

    adding comments

--- challenge_01/challenge.py
+++ challenge_01/challenge.py
@@ -1,20 +1,26 @@
+#Load the inputs
 f = open("input.txt", "r")
 
+#Instantiate the digit strings
 words =  [None,'one','two','three','four','five','six','seven','eight','nine']
 
 lines = []
 
+#Loop through input lines
 for x in f:
   print(x)
+  #init first/last placeholders for each line
   first_idx = len(x) + 999
   first = None
   last_idx = -1
   last = None
 
   for i in range(1,10):
+    # Search for digit or number from left
     dig_idx = x.find(str(i))
     wrd_idx = x.find(words[i])
 
+    #Save if found to left of first_idx
     if dig_idx > -1:
       if dig_idx < first_idx:
         first_idx = dig_idx
@@ -25,9 +31,11 @@ for x in f:
         first_idx = wrd_idx
         first = i
   
+    # Search for digit or number from right
     dig_idx = x.rfind(str(i))
     wrd_idx = x.rfind(words[i])
 
+    #Save if found to right of first_idx
     if dig_idx > -1:
       if dig_idx > last_idx:
         last_idx = dig_idx
@@ -38,15 +46,16 @@ for x in f:
         last_idx = wrd_idx
         last = i
       
-    print(f'first={first} last={last}')
+    print(f'{i}: first={first} last={last}')
 
   if first != None:
     line = int(f"{first}{last}")
     print(f"{x} : {line}")
+    #Concat & Store Result
     lines.append(line)
 
   
   # break
   
-
+#Sum lines
 print(sum(lines))
