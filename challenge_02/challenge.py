6b47a79e1055d87b584601c178798ec6be8025a4
rfe123 <59402267+rfe123@users.noreply.github.com>
2024-09-27 00:19:23 -0500

    update for part2

--- challenge_02/challenge.py
+++ challenge_02/challenge.py
@@ -8,12 +8,19 @@ max_colors = {
 }
 
 good_games = []
+min_cube_powers = []
 
 for x in f:
     row = str.split(x, ':')
 
     game_id = int(str(row[0]).split(' ')[1])
     games = str(row[1]).split(';')
+
+    game_mins = {
+        'red': 0,
+        'green': 0,
+        'blue': 0
+    }
 
     print(game_id)
     valid_game = True
@@ -24,20 +31,23 @@ for x in f:
         for c in colors:
             color = c.strip().split(' ')
             count = int(color[0])
-
-            print(color)
             
-            if count > max_colors[color[1]]:
-                valid_game = False
-                print(f'too many {color[1]}')
-                break
+            if count > game_mins[color[1]]:
+                # valid_game = False
+                # print(f'too many {color[1]}')
+                # break
+                game_mins[color[1]] = count
 
-        if valid_game == False:
-            break
+        # if valid_game == False:
+        #     break
         
-    print(valid_game)
-    if valid_game:
-        good_games.append(game_id)
+    # print(valid_game)
+    # if valid_game:
+    #     good_games.append(game_id)
+    print(games)
+    print(game_mins)
 
-print(sum(good_games))
+    min_cube_powers.append(game_mins['red'] * game_mins['green'] * game_mins['blue'])
+
+print(sum(min_cube_powers))
 
